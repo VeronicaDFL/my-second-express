629fa0b8be3b212c3e04b283edc9b5ff2852e41d
Veronica De Felice <verodefelic@gmail.com>
2023-04-03 15:52:27 -0400

    FIRST commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const PORT = 3010;
+
+const app = express();
+ app.get ('/', (req, res) => {
+ res.send(" I am Index!");
+
+ }) ;
+
+
+app.get ("/hello",(req, res) => {
+res.send("Hello there!");
+
+});
+app.listen(PORT, () => {
+
+    console.log(`Listening on port http://localhost:${PORT}.`);
+});
+
